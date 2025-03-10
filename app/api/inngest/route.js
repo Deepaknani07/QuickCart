8060129cfa7808073c31410d2481b611e6fa641c
Deepaknani07 <nanideepak05@gmail.com>
2025-03-10 13:23:25 +0530

    hmm

---
+++ app/api/inngest/route.js
@@ -0,0 +1,13 @@
+import { serve } from "inngest/next";
+import { inngest, syncUserCreation, syncUserDeletion, syncUserupdation } from "@/config/inngest";
+
+export const { GET, POST, PUT } = serve({
+    client: inngest,
+    functions: [
+        syncUserCreation,
+        syncUserupdation,
+        syncUserDeletion
+
+
+    ],
+});
